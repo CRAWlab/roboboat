3537213f10bf2d2680a0a7ce1cc0f6954e3a860d
Adam Smith <C00012796@louisiana.edu>
2021-05-12 14:59:29 -0500

    added sysID package no clue if this works or is done right.
    my VM was having issues so I had to reinstall. This was taking forever so instead I copied an existing package and edited the copied files to create this package.

---
+++ sysID/scripts/pure_yaw7.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+import rospy
+from geometry_msgs.msg import Twist
+# Initialize the node
+rospy.init_node('sysID_vel_commands_yaw7', anonymous=True)
+# Create a publisher to publish cmd_vel tell it to move
+cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=0)
+# 10 Hz update rate - update to match your setup
+rate = rospy.Rate(10)
+# Define the move command's Twist as a Twist message
+move_cmd = Twist()
+seq=0
+while not rospy.is_shutdown():        
+        # assign the translational velocity commands
+        move_cmd.linear.x = 0
+        move_cmd.linear.y = 0
+        # assign the angular velocity command
+        move_cmd.angular.z = 7.2
+        #stop moving after 60seconds
+        if seq>600:
+            move_cmd.linear.x = 0
+            move_cmd.linear.y = 0
+            move_cmd.angular.z = 0
+        # Then publish it
+        cmd_vel.publish(move_cmd)
+        rate.sleep()
+        cmd_vel.publish(move_cmd)
+        seq+=1
+except (KeyboardInterrupt, SystemExit):
+    cmd_vel.publish(move_cmd) # publish all zeros to stop
